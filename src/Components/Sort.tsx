af325b2a6943672d82ab16609679029b4baf9dba
Kaitlyn Katz <64869151+kaitlynkatz@users.noreply.github.com>
2020-12-18 15:39:02 -0500

    extract logic to handleChange

--- src/Components/Sort.tsx
+++ src/Components/Sort.tsx
@@ -44,24 +44,24 @@ export default class Sort extends React.Component {
     this.setState(this.headlessSort.state);
   }
 
+  handleChange(event: any) {
+    switch (event.target.value) {
+      case "relevance":
+        this.headlessSort.sortBy(this.relevanceSortCriterion);
+        break;
+      case "datedescending":
+        this.headlessSort.sortBy(this.dateDescendingSortCriterion);
+        break;
+      default:
+        this.headlessSort.sortBy(this.dateAscendingSortCriterion);
+        break;
+    }
+  }
+
   render() {
     return (
       <FormControl>
-        <NativeSelect
-          onChange={(event: any) => {
-            switch (event.target.value) {
-              case "relevance":
-                this.headlessSort.sortBy(this.relevanceSortCriterion);
-                break;
-              case "datedescending":
-                this.headlessSort.sortBy(this.dateDescendingSortCriterion);
-                break;
-              default:
-                this.headlessSort.sortBy(this.dateAscendingSortCriterion);
-                break;
-            }
-          }}
-        >
+        <NativeSelect onChange={(event: any) => this.handleChange(event)}>
           <option value="relevance">Relevance</option>
           <option value="datedescending">Date Descending</option>
           <option value="dateascending">Date Ascending</option>
