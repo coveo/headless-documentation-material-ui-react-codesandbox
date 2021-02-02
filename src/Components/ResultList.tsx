a0e81bf9966cfff0572d4027d1f5ee4939218f5c
Frederic Beaudoin <fbeaudoin@coveo.com>
2021-02-02 15:42:18 -0500

    Use ResultLink to log click events when result is clicked

--- src/Components/ResultList.tsx
+++ src/Components/ResultList.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-use-before-define */
 import React from "react";
 import List from "@material-ui/core/List";
-import { ListItem, ListItemText, Box } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import ResultLink from "./ResultLink"
 import Divider from "@material-ui/core/Divider";
 import {
   buildResultList,
@@ -12,10 +13,6 @@ import {
   ResultListState
 } from "@coveo/headless";
 import { headlessEngine } from "../Engine";
-
-function ListItemLink(props: any) {
-  return <ListItem button component="a" {...props} />;
-}
 
 export default class ResultList extends React.Component {
   private headlessResultList: ResultListType;
@@ -36,11 +33,7 @@ export default class ResultList extends React.Component {
       conditions: [],
       content: (result: Result) => (
         <Box key={result.uniqueId}>
-          <ListItem disableGutters>
-            <ListItemLink disableGutters href={result.clickUri} target="_blank">
-              <ListItemText primary={result.title} secondary={result.excerpt} />
-            </ListItemLink>
-          </ListItem>
+          <ResultLink result={result} />
           <Divider />
         </Box>
       )
