fe9aebe30c3028eae4cecbef58ae57a0772f78a0
jpmarceau <39384459+jpmarceau@users.noreply.github.com>
2023-04-14 14:25:08 -0400

    getOrganizationEndpoints

--- src/Engine.tsx
+++ src/Engine.tsx
@@ -1,9 +1,10 @@
-import { buildSearchEngine, buildContext } from "@coveo/headless";
+import { buildSearchEngine, buildContext, getOrganizationEndpoints } from "@coveo/headless";
 
 const headlessEngine = buildSearchEngine({
   configuration: {
     organizationId: "barcagroupproductionkwvdy6lp",
     accessToken: "xx5a7943ef-ea52-42e5-8742-51198cc651f7",
+    organizationEndpoints: getOrganizationEndpoints('barcagroupproductionkwvdy6lp')
   },
 });
 
