eed6b7b1ed717c6b95b17051b6e029a9543f5bbe
Jean-Philippe Marceau <39384459+jpmarceau@users.noreply.github.com>
2024-08-21 10:35:58 -0400

    next analytics

--- src/Engine.tsx
+++ src/Engine.tsx
@@ -4,7 +4,8 @@ const headlessEngine = buildSearchEngine({
   configuration: {
     organizationId: "barcagroupproductionkwvdy6lp",
     accessToken: "xx5a7943ef-ea52-42e5-8742-51198cc651f7",
-    organizationEndpoints: getOrganizationEndpoints('barcagroupproductionkwvdy6lp')
+    organizationEndpoints: getOrganizationEndpoints('barcagroupproductionkwvdy6lp'),
+    analytics: {analyticsMode: 'next', trackingId: 'engineering'},
   },
 });
 
